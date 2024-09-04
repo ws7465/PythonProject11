9855aecec067fea568e8ba6acdc4c22ca91b44ed
ws7465 <ws7465@ya.ru>
2024-09-04 15:57:08 +0300

    2024/09/04 - урок 1

--- main.py
+++ main.py
@@ -14,3 +14,13 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# 1. Количество выполненных ДЗ (запишите значение 12)
+col_vyip_DZ = 12
+# 2. Количество затраченных часов (запишите значение 1.5)
+col_zatr_chas = 1.5
+# 3. Название курса (запишите значение 'Python')
+name_of_course = 'Python'
+# 4. Время на одно задание (вычислить используя 1 и 2 переменные)
+time_of_1s_zadaniye = 1.5/12
+# 5. всё
